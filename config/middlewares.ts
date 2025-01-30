fb56922fb2d99cb99f9bd830c229d0fcae7b4f26
Takamanu <fauzanalivijsma@gmail.com>
2025-01-30 23:11:20 +0700

    push ke env prod rivandi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,39 +1,40 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
   // 'strapi::security',
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'minio.garnespratama.xyz',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "minio.garnespratama.xyz",
+            "minio.rivandi.blog",
           ],
-          'media-src': [
+          "media-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'minio.garnespratama.xyz',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "minio.garnespratama.xyz",
+            "minio.rivandi.blog",
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-
 ];
--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,36 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
-    'strapi::logger',
-    'strapi::errors',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
     // 'strapi::security',
     {
-        name: 'strapi::security',
+        name: "strapi::security",
         config: {
             contentSecurityPolicy: {
                 useDefaults: true,
                 directives: {
-                    'connect-src': ["'self'", 'https:'],
-                    'img-src': [
+                    "connect-src": ["'self'", "https:"],
+                    "img-src": [
                         "'self'",
-                        'data:',
-                        'blob:',
-                        'market-assets.strapi.io',
-                        'minio.garnespratama.xyz',
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "minio.garnespratama.xyz",
+                        "minio.rivandi.blog",
                     ],
-                    'media-src': [
+                    "media-src": [
                         "'self'",
-                        'data:',
-                        'blob:',
-                        'market-assets.strapi.io',
-                        'minio.garnespratama.xyz',
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "minio.rivandi.blog",
                     ],
                     upgradeInsecureRequests: null,
                 },
